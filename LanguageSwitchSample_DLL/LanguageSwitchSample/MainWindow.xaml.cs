700dfcaed26f82d11584234711a4c6ace7aa4ca2
unknown <Takt_Visitor7@VISITOR7.takt.local>
2023-01-20 14:03:25 +0900

    add language sw dll sample

---
+++ LanguageSwitchSample_DLL/LanguageSwitchSample/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Windows;
+using WpfControlLibrary1;
+
+namespace LanguageSwitchSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Class1 class1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            class1 = new Class1(uc1);
+        }
+
+        private void Button_en_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeLang(@"lang/english.xaml");
+            class1.LanguageSwitch(true);
+        }
+
+        private void ChangeLang(string path)
+        {
+            ResourceDictionary langRd = null;
+            string langFile = path;
+
+            try
+            {
+                langRd = Application.LoadComponent(new Uri(langFile, UriKind.Relative)) as ResourceDictionary;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            if (langRd != null)
+            {
+                if (this.Resources.MergedDictionaries.Count > 0)
+                {
+                    //一つのリソースディレクトリを使うとき、
+                    //クリアした後、追加するのはOK
+                    //複数のリソースディレクトリを使うとき、
+                    //クリアすると、他のディレクトリがなくなるので
+                    //MergedDictionaries[index]を固定して、修正したほうがいい
+                    //w.Resources.MergedDictionaries.Clear();
+                    this.Resources.MergedDictionaries[0] = langRd;
+                }
+                else
+                {
+                    this.Resources.MergedDictionaries.Add(langRd);
+                }
+            }
+        }
+
+        private void Button_ja_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeLang(@"lang/japanses.xaml");
+            class1.LanguageSwitch(false);
+        }
+    }
+}
---
+++ LanguageSwitchSample_DLL/WpfControlLibrary1/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfControlLibrary1
+{
+    public interface IWindowInfo
+    {
+        public bool LanguageSwitch(bool isEnglish);
+    }
+
+    public class Class1:IWindowInfo
+    {
+        private UserControl1 control;
+
+        public Class1(UserControl1 obj)
+        {
+            control = obj;
+        }
+
+        public bool LanguageSwitch(bool isEnglish)
+        {
+            ResourceDictionary? langRd = null;
+            string langFile = "";
+
+            if (isEnglish)
+            {
+                langFile = "lang/en.xaml";
+            }
+            else
+            {
+                langFile = "lang/jp.xaml";
+            }
+
+            try
+            {
+                langRd = Application.LoadComponent(new Uri("/WpfControlLibrary1;component/" + langFile, UriKind.Relative)) as ResourceDictionary;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+
+            if (langRd != null)
+            {
+                if (control.Resources.MergedDictionaries.Count > 0)
+                {
+                    control.Resources.MergedDictionaries[0] = langRd;
+                }
+                else
+                {
+                    control.Resources.MergedDictionaries.Add(langRd);
+                }
+            }
+
+            return true;
+        }
+    }
+}
